a49ce90cd4e25a783303dac67bafd5697234a011
Eric <2ericleroux@gmail.com>
2023-12-06 17:11:52 -0500

    fixed adjusted neighbours

--- recommender_system.py
+++ recommender_system.py
@@ -7,7 +7,7 @@ class RecommenderSystem:
     '''
     RecommenderSystem class for COMP4601 Assignment 2.
     '''
-    MISSING_RATING = 0
+    MISSING_RATING = 0.0
     DEFAULT_NEIGHBOURHOOD_SIZE = 5
     DEFAULT_SIMILARITY_THRESHOLD = 0.5
     ITEM_BASED_ALGORITHM = 0
@@ -368,48 +368,53 @@ class RecommenderSystem:
         try:
             # Get all neighbors who rated the item, adjust neighborhood size if necessary
             neighbours = np.where(self.ratings[:, item_index] != self.MISSING_RATING)[0]
-            adjusted_neighbourhood_size = min(self.neighbourhood_size, len(neighbours))
 
             print(f"\nPredicting for user: {self.users[user_index]}")
             print(f"Predicting for item: {self.items[item_index]}")
 
             # If no neighbors found, use the average rating for the user
+            
+            # Create array of tuples using neighbors indices and similarities
+            if self.include_negative_correlations:
+                neighborhood_similarities = [(num, i, i in neighbours) for i, num in enumerate(abs(similarities[user_index, :]))]
+            else:
+                neighborhood_similarities = [(num, i, i in neighbours) for i, num in enumerate(similarities[user_index, :]) if num > 0]
+            adjusted_neighbourhood_size = min(self.neighbourhood_size, len(neighborhood_similarities))
+            # Sort array based on similarities in descending order for neighborhood similarities
+            sorted_indices = np.array([index for _, index, is_in_array in sorted(neighborhood_similarities, key=lambda x: (x[0], -x[1]), reverse=True) if is_in_array])
+            
+            # Filtering technique
+            filtered_indices = []
+            if self.filter_type == self.SIMILARITY_THRESHOLD:
+                filtered_indices = sorted_indices[sorted_indices > self.similarity_threshold]
+            elif self.filter_type == self.TOP_K_NEIGHBOURS:
+                filtered_indices = sorted_indices[:adjusted_neighbourhood_size]
+            else:
+                filtered_indices = sorted_indices[sorted_indices > self.similarity_threshold][:adjusted_neighbourhood_size]
+
+            adjusted_neighbourhood_size = len(filtered_indices)
+            
             if adjusted_neighbourhood_size == 0:
+                print(f"Found no valid neighbours:")
                 ratings_without_zeros = np.where(self.ratings[user_index] != 0, self.ratings[user_index], np.nan)
                 predict_rating = np.nanmean(ratings_without_zeros)
                 total_similarity = 0
             else:
-                # Create array of tuples using neighbors indices and similarities
-                if self.include_negative_correlations:
-                    neighborhood_similarities = [(num, i, i in neighbours) for i, num in enumerate(abs(similarities[user_index, :]))]
-                else:
-                    neighborhood_similarities = [(num, i, i in neighbours) for i, num in enumerate(similarities[user_index, :])]
-
-                # Sort array based on similarities in descending order for neighborhood similarities
-                sorted_indices = np.array([index for _, index, is_in_array in sorted(neighborhood_similarities, key=lambda x: (x[0], -x[1]), reverse=True) if is_in_array])
-
-                # Filtering technique
-                if self.filter_type == self.SIMILARITY_THRESHOLD:
-                    filtered_indices = sorted_indices[sorted_indices > self.similarity_threshold]
-                elif self.filter_type == self.TOP_K_NEIGHBOURS:
-                    filtered_indices = sorted_indices[:adjusted_neighbourhood_size]
-                else:
-                    filtered_indices = sorted_indices[sorted_indices > self.similarity_threshold][:adjusted_neighbourhood_size]
-                    
-                print(f"Found {len(filtered_indices)} valid neighbours:")
+                # print(similarities)
+                print(f"Found {adjusted_neighbourhood_size} valid neighbours:")
                 for i, idx in enumerate(filtered_indices):
                     print(f"{i + 1}. User {self.users[idx]} sim={similarities[user_index, idx]}")
 
                 # Calculate the predicted rating using the given formula
                 numerator = 0
                 denominator = 0
-                print(filtered_indices)
+                # print(filtered_indices)
                 for idx in filtered_indices:
                     similarity = similarities[user_index, idx]
                     rating_diff = self.ratings[idx, item_index] - average_ratings[idx]
                     numerator += similarity * rating_diff
                     denominator += abs(similarity)
-                print(f"{numerator}/{denominator}")
+                # print(f"{numerator}/{denominator}")
                 if denominator == 0:
                     # Handle the case where the denominator is zero to avoid division by zero
                     predict_rating = average_ratings[user_index]
